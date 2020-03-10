281c2ae233f04361bc4d9a16df76fc0d3c416e9e
Joe Pea <joe@trusktr.io>
2020-03-10 00:01:18 -0700

    move builder scripts to lume scripts, and remove builder stuff

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import {variable, autorun, reactive} from './index'
+
+describe('variable', () => {
+	it('needs testing', () => {
+		expect(variable).toBeInstanceOf(Function)
+		expect(autorun).toBeInstanceOf(Function)
+		expect(reactive).toBeInstanceOf(Function)
+	})
+})
