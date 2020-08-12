2563fca80183f947f661fcb42ca58007685415da
Joe Pea <joe@trusktr.io>
2020-08-11 17:20:43 -0700

    export solid-js/html too, so that @lume/element can export the html template tag and thus not require a build step

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import {createSignal, createEffect, createRoot} from 'solid-js'
 
 export * from 'solid-js'
+
+// These lines should live in @lume/element, but doing that causes Solid to
+// exist more than once in node_modules, which causes issues. So because we know
+// @lume/element depends on @lume/variable, we stuck these here to avoid the
+// duplicate-module issues.
 export * from 'solid-js/dom'
+export * from 'solid-js/html'
 
 export interface ReactiveVariable<T> {
 	(value?: undefined): T
