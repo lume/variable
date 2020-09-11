ff0613ab5c4986a05c06e4563445accae84f5065
Joe Pea <joe@trusktr.io>
2020-09-11 00:07:03 -0700

    ensure and test that decorators work in all 2 TypeScript and 4 Babel decorator build configurations, effectively allowing decorators to work with and without class field [[Define]] semantics.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,187 @@
-import {variable, autorun, reactive} from './index'
+import {variable, autorun, reactive, reactify} from './index'
 
-describe('variable', () => {
-	it('needs testing', () => {
-		expect(variable).toBeInstanceOf(Function)
-		expect(autorun).toBeInstanceOf(Function)
-		expect(reactive).toBeInstanceOf(Function)
+describe('@lume/variable', () => {
+	describe('variable()', () => {
+		it('has gettable and settable values', async () => {
+			expect(variable).toBeInstanceOf(Function)
+			const num = variable(0)
+
+			// Set the variable's value by passing a value in.
+			num(1)
+
+			// Read the variable's value by calling it with no args.
+			expect(num()).toBe(1)
+
+			// increment example:
+			num(num() + 1)
+
+			expect(num()).toBe(2)
+
+			// An alternative way to set and get the variable's values, if you
+			// prefer.
+			num.set(3)
+			expect(num.get()).toBe(3)
+
+			// The get and set methods are useful, for example, if you'd like to expose only
+			// one or the other to external code, but not both (making
+			// them write-only or read-only).
+
+			let count: () => number
+
+			// Example: The following block scope exposes only the getter.
+			{
+				const {get, set} = variable(0)
+
+				// Expose the getter to the outside.
+				count = get
+
+				Promise.resolve().then(() => {
+					set(1)
+					set(count() + 1)
+					set(3)
+				})
+			}
+
+			// On the outside, we can only read the variable's value.
+			let expectedCount = -1
+			autorun(() => {
+				expectedCount++
+				expect(count()).toBe(expectedCount)
+			})
+
+			await Promise.resolve()
+
+			expect(count()).toBe(3)
+			expect(expectedCount).toBe(3)
+		})
+	})
+
+	describe('autorun()', () => {
+		it('re-runs on changes of variables used within, and can be stopped from re-running', () => {
+			expect(autorun).toBeInstanceOf(Function)
+
+			const count = variable(0)
+
+			let runCount = 0
+
+			// Runs once immediately, then re-runs any time the variables used
+			// within have changed (in this case, only the count() variable).
+			const stop = autorun(() => {
+				count()
+				runCount++
+			})
+
+			count(1)
+			count(2)
+
+			expect(runCount).toBe(3)
+
+			// Stops the autorun from re-running. It can now be garbage collected.
+			stop()
+
+			count(3)
+			count(4)
+
+			// The runCount is still the same because the autorun didn't re-run.
+			expect(runCount).toBe(3)
+		})
+	})
+
+	describe('@reactive', () => {
+		it('is a function', () => {
+			expect(reactive).toBeInstanceOf(Function)
+		})
+
+		it('makes class properties reactive, using class and property/accessor decorators', () => {
+			@reactive
+			class Butterfly {
+				@reactive colors = 3
+
+				_wingSize = 2
+
+				@reactive
+				get wingSize() {
+					return this._wingSize
+				}
+				set wingSize(s: number) {
+					this._wingSize = s
+				}
+			}
+
+			const b = new Butterfly()
+
+			testButterflyProps(b)
+		})
+
+		it('makes class properties reactive, using only property/accessor decorators', () => {
+			class Butterfly {
+				@reactive colors = 3
+
+				_wingSize = 2
+
+				@reactive
+				get wingSize() {
+					return this._wingSize
+				}
+				set wingSize(s: number) {
+					this._wingSize = s
+				}
+
+				constructor() {
+					reactify(this, Butterfly)
+				}
+			}
+
+			const b = new Butterfly()
+
+			testButterflyProps(b)
+		})
+
+		it('makes class properties reactive, not using any decorators', () => {
+			class Butterfly {
+				colors = 3
+
+				_wingSize = 2
+
+				get wingSize() {
+					return this._wingSize
+				}
+				set wingSize(s: number) {
+					this._wingSize = s
+				}
+
+				constructor() {
+					reactify(this, ['colors', 'wingSize'])
+				}
+			}
+
+			const b = new Butterfly()
+
+			testButterflyProps(b)
+		})
 	})
 })
+
+function testButterflyProps(b: any) {
+	let count = 0
+
+	autorun(() => {
+		b.colors
+		b.wingSize
+		count++
+	})
+
+	expect(b.colors).toBe(3)
+	expect(b.wingSize).toBe(2)
+	expect(count).toBe(1)
+
+	b.colors++
+
+	expect(b.colors).toBe(4)
+	expect(count).toBe(2)
+
+	b.wingSize++
+
+	expect(b.wingSize).toBe(3)
+	expect(count).toBe(3)
+}
--- src/index.ts
+++ src/index.ts
@@ -108,20 +108,91 @@ function __getReactiveVar<T>(instance: Object, vName: string, initialValue: T = 
 	return v
 }
 
-export function reactive<T>(prototype: any, name: string, descriptor?: PropertyDescriptor): any {
+export function reactive(protoOrClassElement: any, name?: string, descriptor?: PropertyDescriptor): any {
+	// If used as a newer Babel decorator
+	const isDecoratorV2 = arguments.length === 1 && 'kind' in protoOrClassElement
+	if (isDecoratorV2) {
+		const classElement = protoOrClassElement
+
+		// If used as a class decorator.
+		if (classElement.kind === 'class') {
+			return {
+				...classElement,
+				finisher(Class: AnyClass) {
+					return class extends Class {
+						constructor() {
+							super()
+							reactify(this, Class)
+						}
+					}
+				},
+			}
+		}
+
+		// If used as a property or accessor decorator (this isn't intended for
+		// methods).
+		return {
+			...classElement,
+			// placement: 'prototype',
+			finisher(Class: AnyClass) {
+				_reactive(Class.prototype, classElement.key /*, classElement.descriptor*/)
+				return classElement.finisher?.(Class) ?? Class
+			},
+		}
+	}
+
+	// Used as a v1 legacy decorator.
+
+	// If used as a class decorator.
+	if (arguments.length === 1 && typeof protoOrClassElement === 'function') {
+		const Class = protoOrClassElement
+
+		return class extends Class {
+			constructor() {
+				super()
+				reactify(this, Class)
+			}
+		}
+	}
+
+	// If used as a property or accessor decorator (this isn't intended for
+	// methods).
+	return _reactive(protoOrClassElement, name!, descriptor)
+}
+
+const classToKeys = new WeakMap<AnyClass, string[]>()
+
+function _reactive(prototype: any, name: string, descriptor?: PropertyDescriptor) {
+	let keys = classToKeys.get(prototype.constructor)
+	if (!keys) classToKeys.set(prototype.constructor, (keys = []))
+	keys.push(name)
+
 	const vName = 'v_' + name
 
-	// property decorators are not passed a descriptor (unlike decorators on accessors or methods)
+	// TODO If prototype already has vName, skip making an accessor.
+	// if (prototype[vName] !== undefined) return
+
 	let calledAsPropertyDecorator = false
 
-	if (!descriptor) {
+	// In TypeScript property decorators are not passed a descriptor (unlike decorators on accessors or methods)
+	// const isTypeScriptPropertyDecorator = !descriptor
+	if (
+		// TypeScript legacy decorator case
+		!descriptor ||
+		// Babel legacy decorator case
+		'initializer' in descriptor
+	) {
 		calledAsPropertyDecorator = true
-		descriptor = Object.getOwnPropertyDescriptor(prototype, name)
+
+		// TypeScript legacy decorator case
+		if (!descriptor) descriptor = Object.getOwnPropertyDescriptor(prototype, name)
+
+		// In the Babel legacy decorator case, descriptor always exists.
 	}
 
 	let originalGet: (() => any) | undefined
 	let originalSet: ((v: any) => void) | undefined
-	let initialValue: T
+	let initialValue: unknown
 	let writable: boolean | undefined
 
 	// TODO if there is an inherited accessor, we need to ensure we still call
@@ -169,9 +240,10 @@ export function reactive<T>(prototype: any, name: string, descriptor?: PropertyD
 		...descriptor,
 		// XXX should we throw an error if descriptor.configurable is false?
 		configurable: true,
-		get(): T {
+		get(): unknown {
 			// initialValue could be undefined
-			const v = __getReactiveVar<T>(this, vName, initialValue)
+			// XXX this causes initialValue to be held onto after subsequent values and not collected
+			const v = __getReactiveVar(this, vName, initialValue)
 
 			if (originalGet) {
 				// track reactivity, but get the value from the original getter
@@ -181,8 +253,8 @@ export function reactive<T>(prototype: any, name: string, descriptor?: PropertyD
 
 			return v()
 		},
-		set(newValue: T) {
-			const v = __getReactiveVar<T>(this, vName, initialValue)
+		set(newValue: unknown) {
+			const v = __getReactiveVar(this, vName, initialValue)
 
 			if (originalSet) originalSet.call(this, newValue)
 
@@ -190,14 +262,48 @@ export function reactive<T>(prototype: any, name: string, descriptor?: PropertyD
 		},
 	}
 
-	// If a decorator is called on a property, then returning a descriptor does
-	// nothing, so we need to set the descriptor manually.
+	// If a TypeScript decorator is called on a property, then returning a descriptor does
+	// nothing, so we need to set the descriptor manually. We'll do it for Babel decorators too.
 	if (calledAsPropertyDecorator) Object.defineProperty(prototype, name, descriptor)
-	// If a decorator is called on an accessor or method, then we must return a
+	// If a TypeScript decorator is called on an accessor or method, then we must return a
 	// descriptor in order to modify it, and doing it manually won't work.
 	else return descriptor
 	// Weird, huh?
 	// This will change with updates to the ES decorators proposal, https://github.com/tc39/proposal-decorators
+}
+
+type AnyClass = new (...args: any[]) => object
+
+export function reactify<T>(obj: T, props: string[]): typeof obj
+export function reactify<C extends AnyClass>(obj: InstanceType<C>, ctor: C): typeof obj
+export function reactify(obj: any, propsOrCtor: any) {
+	if (typeof propsOrCtor === 'function') {
+		const ctor = propsOrCtor
+		const o = obj as Record<string, unknown>
+		const keys = classToKeys.get(ctor)
+
+		if (keys) {
+			for (const key of keys) {
+				if (obj.hasOwnProperty(key)) {
+					const initialValue = o[key]
+					delete o[key]
+					o[key] = initialValue
+				}
+			}
+		}
+
+		return obj
+	}
+
+	const keys = propsOrCtor
+
+	for (const key of keys) {
+		const initialValue = obj[key]
+		_reactive(obj, key)
+		obj[key] = initialValue
+	}
+
+	return obj
 }
 
 export const version = '0.1.0'
