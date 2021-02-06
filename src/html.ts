233aef36a764ce6ad3599f2a5611a5e21631d180
Joe Pea <joe@trusktr.io>
2021-02-05 22:19:48 -0800

    fix import of html for node ESM (temporary until solid-js/html updates)

--- src/html.ts
+++ src/html.ts
@@ -2,7 +2,9 @@
 // more than once in node_modules, which causes duplicate-module issues. So
 // because we know @lume/element depends on @lume/variable, we stuck this here
 // to avoid the duplicate-module issues.
-import _html from 'solid-js/html'
+// @ts-ignore
+import _html from 'solid-js/html/dist/html.js'
+import type __html from 'solid-js/html'
 
 import {getGlobal} from './getGlobal.js'
 
@@ -12,6 +14,6 @@ const global = getGlobal() as any
 // a global var. If there are any duplicate modules, they will use the lib that
 // is stored on the global var instead of using a lib that is encpsulated in
 // (and unique to) their ES Module imports.
-const html = global.SOLID_HTML ?? (global.SOLID_HTML = _html)
+const html = (global.SOLID_HTML ?? (global.SOLID_HTML = _html)) as typeof __html
 
 export {html}
