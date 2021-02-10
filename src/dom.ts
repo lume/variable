1f2bc410bdf7404484949f8f3a7de98ef83f12be
Joe Pea <joe@trusktr.io>
2021-02-10 11:13:48 -0800

    update solid-js
    New solid re-named "dom" to "web".

--- src/dom.ts
+++ src/dom.ts
@@ -3,16 +3,26 @@
 // because we know @lume/element depends on @lume/variable, we stuck this here
 // to avoid the duplicate-module issues.
 
-import * as _dom from 'solid-js/dom'
+import * as _web from 'solid-js/web'
 
 import {getGlobal} from './getGlobal.js'
 
 const global = getGlobal() as any
 
+if (global.SOLID_DOM) {
+	// TODO Add a link to a documentation page with more details.
+	console.warn(`
+        Duplicates of a LUME library or dependency detected, conflict avoided.
+        Having duplicates means things may not work as expected if different
+        parts of the application are using different instances of something
+        that should be a singleton.
+    `)
+}
+
 // Another strategy to avoid duplicate modules is to store the improted libs on
 // a global var. If there are any duplicate modules, they will use the lib that
 // is stored on the global var instead of using a lib that is encpsulated in
 // (and unique to) their ES Module imports.
-const dom: typeof _dom = global.SOLID_DOM ?? (global.SOLID_DOM = _dom)
+const dom: typeof _web = global.SOLID_DOM ?? (global.SOLID_DOM = _web)
 
 export {dom}
--- src/html.ts
+++ src/html.ts
@@ -10,6 +10,16 @@ import {getGlobal} from './getGlobal.js'
 
 const global = getGlobal() as any
 
+if (global.SOLID_HTML) {
+	// TODO Add a link to a documentation page with more details.
+	console.warn(`
+        Duplicates of a LUME library or dependency detected, conflict avoided.
+        Having duplicates means things may not work as expected if different
+        parts of the application are using different instances of something
+        that should be a singleton.
+    `)
+}
+
 // Another strategy to avoid duplicate modules is to store the improted libs on
 // a global var. If there are any duplicate modules, they will use the lib that
 // is stored on the global var instead of using a lib that is encpsulated in
