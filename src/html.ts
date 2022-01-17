4e92cf092099a2ff47fb3d222db28a5c1d2c1ab5
Joe Pea <trusktr@gmail.com>
2022-01-17 10:48:26 -0800

    feat: restore named html export

--- src/html.ts
+++ src/html.ts
@@ -4,3 +4,6 @@ import html from 'solid-js/html/dist/html.js'
 import type HTMLTag from 'solid-js/html'
 
 export default html as typeof HTMLTag
+
+const _html = html as typeof HTMLTag
+export {_html as html}
